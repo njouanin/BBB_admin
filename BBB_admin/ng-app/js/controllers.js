761477337e9cc5fb0b99f7b81f8a806975da07b6
Nico <nicolas.jouanin@gmail.com>
2014-05-08 21:51:40 +0200

    Add uptime + badges on network interfaces

--- BBB_admin/ng-app/js/controllers.js
+++ BBB_admin/ng-app/js/controllers.js
@@ -3,8 +3,11 @@ var BBBAdminControllers = angular.module('BBBAdminControllers', []);
 BBBAdminControllers.controller('SystemUnameCtrl', ['$scope', '$http',
      function ($scope, $http) {
         $http.get('/api/system/uname').success(function(data) {
-        $scope.systemInfo = data;
-    });
+          $scope.systemInfo = data;
+        });
+        $http.get('/api/system/uptime').success(function(data) {
+          $scope.uptime = data;
+        });
   }]);
 
 BBBAdminControllers.controller('NetIfacesCtrl', ['$scope', '$http',
